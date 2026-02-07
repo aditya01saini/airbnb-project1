d87051024b1ae5712d2c3b8315e33cf25e4f5d92
Dev aadi <adityasaini35947@gmail.com>
2026-02-08 00:51:26 +0530

    update app.js

--- app.js
+++ app.js
@@ -1,75 +1,194 @@
+// if (process.env.NODE_ENV !== "production") {
+//   require("dotenv").config();
+// }
+
+// const express = require("express");
+// const app = express();
+// const mongoose = require("mongoose");
+// const path = require("path");
+// const methodOverride = require("method-override");
+// const ejsMate = require("ejs-mate");
+// const session = require("express-session");
+// const flash = require("connect-flash");
+// const passport = require("passport");
+// const LocalStrategy = require("passport-local");
+// const MongoStore = require("connect-mongo");
+// const User = require("./models/user.js");
+
+// const listingRouter = require("./routes/listing.js");
+// const reviewsRouter = require("./routes/review.js");
+// const userRouter = require("./routes/user.js");
+
+// const dbUrl = process.env.ATLASDB_URL;
+// if (!dbUrl) {
+//   throw new Error("ATLASDB_URL is not defined");
+// }
+
+// mongoose.connect(dbUrl)
+//   .then(() => console.log("connected to DB"))
+//   .catch(err => console.log(err));
+
+// app.engine("ejs", ejsMate);
+// app.set("view engine", "ejs");
+// app.set("views", path.join(__dirname, "views"));
+
+// app.use(express.urlencoded({ extended: true }));
+// app.use(methodOverride("_method"));
+// app.use(express.static(path.join(__dirname, "public")));
+
+// const store = MongoStore.create({
+//   mongoUrl: dbUrl,
+//   crypto: { secret: process.env.SECRET },
+//   touchAfter: 24 * 3600,
+// });
+
+// store.on("error", () => {
+//   console.log("ERROR in MONGO SESSION STORE");
+// });
+
+// // app.use(session({
+// //   store,
+// //   secret: process.env.SECRET,
+// //   resave: false,
+// //   saveUninitialized: true,
+// //   cookie: {
+// //     httpOnly: true,
+// //     maxAge: 7 * 24 * 60 * 60 * 1000,
+// //   },
+// // }));
+
+// app.use(session({
+//   secret: process.env.SECRET,
+//   resave: false,
+//   saveUninitialized: true,
+// }));
+
+// app.use(flash());
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+// passport.use(new LocalStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+
+// app.use((req, res, next) => {
+//   res.locals.success = req.flash("success");
+//   res.locals.error = req.flash("error");
+//   res.locals.currUser = req.user;
+//   next();
+// });
+
+// app.use("/listings", listingRouter);
+// app.use("/listings/:id/reviews", reviewsRouter);
+// app.use("/", userRouter);
+
+// app.use((err, req, res, next) => {
+//   console.error(err);
+//   const { statusCode = 500, message = "Something went wrong" } = err;
+//   res.status(statusCode).render("error.ejs", { message });
+// });
+
+// const PORT = process.env.PORT || 8080;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
+
 const express = require("express");
 const app = express();
+
 const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+
 const session = require("express-session");
 const flash = require("connect-flash");
+
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
+
 const MongoStore = require("connect-mongo");
+
 const User = require("./models/user.js");
+
 
 const listingRouter = require("./routes/listing.js");
 const reviewsRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
+
 const dbUrl = process.env.ATLASDB_URL;
-if (!dbUrl) {
-  throw new Error("ATLASDB_URL is not defined");
+const secret = process.env.SECRET;
+
+if (!dbUrl || !secret) {
+  console.error("Missing ATLASDB_URL or SECRET in .env");
+  process.exit(1);
 }
 
-mongoose.connect(dbUrl)
-  .then(() => console.log("connected to DB"))
-  .catch(err => console.log(err));
+mongoose
+  .connect(dbUrl)
+  .then(() => console.log("✅ Connected to MongoDB"))
+  .catch((err) => {
+    console.error("MongoDB Error:", err);
+    process.exit(1);
+  });
+
 
 app.engine("ejs", ejsMate);
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+
 app.use(express.urlencoded({ extended: true }));
+
 app.use(methodOverride("_method"));
+
 app.use(express.static(path.join(__dirname, "public")));
+
 
 const store = MongoStore.create({
   mongoUrl: dbUrl,
-  crypto: { secret: process.env.SECRET },
-  touchAfter: 24 * 3600,
+  secret: secret,
+  touchAfter: 24 * 60 * 60, 
 });
 
-store.on("error", () => {
-  console.log("ERROR in MONGO SESSION STORE");
+store.on("error", (err) => {
+  console.error("Session Store Error:", err);
 });
 
-// app.use(session({
-//   store,
-//   secret: process.env.SECRET,
-//   resave: false,
-//   saveUninitialized: true,
-//   cookie: {
-//     httpOnly: true,
-//     maxAge: 7 * 24 * 60 * 60 * 1000,
-//   },
-// }));
 
-app.use(session({
-  secret: process.env.SECRET,
+const sessionConfig = {
+  store,
+  name: "airbnb-session",
+  secret: secret,
   resave: false,
-  saveUninitialized: true,
-}));
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true,
+    maxAge: 7 * 24 * 60 * 60 * 1000, 
+  },
+};
+
+app.use(session(sessionConfig));
 
 app.use(flash());
 
+
 app.use(passport.initialize());
 app.use(passport.session());
+
 passport.use(new LocalStrategy(User.authenticate()));
+
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
 
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
@@ -78,17 +197,35 @@ app.use((req, res, next) => {
   next();
 });
 
+
+app.get("/", (req, res) => {
+  res.redirect("/listings");
+});
+
+
 app.use("/listings", listingRouter);
+
 app.use("/listings/:id/reviews", reviewsRouter);
+
 app.use("/", userRouter);
 
+
 app.use((err, req, res, next) => {
-  console.error(err);
-  const { statusCode = 500, message = "Something went wrong" } = err;
+  console.error("ERROR:", err);
+
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Something went wrong";
+
+  if (res.headersSent) {
+    return next(err);
+  }
+
   res.status(statusCode).render("error.ejs", { message });
 });
 
+
 const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
